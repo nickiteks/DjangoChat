79994a9f44291f3a53eba80a3f24654bea431803
NULS <spint38@gmail.com>
2022-10-01 02:11:14 +0400

    add some functions

--- public_chat/base/urls.py
+++ public_chat/base/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('add/',views.add_post),
+#TODO 43 49
 ]
--- public_chat/base/views.py
+++ public_chat/base/views.py
@@ -1,7 +1,22 @@
 from multiprocessing import context
 from django.shortcuts import render
+from .models import Post
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 def index(request):
-    context = {}
+    posts = Post.objects.all()
+
+    context = {'Posts':posts}
     return render(request, "index.html",context)
+
+@api_view
+def add_post(request):
+    data = request.data
+    print('Data',data)
+    post = Post.objects.create(
+        bodu=data.body
+    )
+
+    return Response('Data was subnitted')
