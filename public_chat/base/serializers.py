f547dddc1a99ae7caca94d589f46d765f7f2699e
nickiteks <55086354+nickiteks@users.noreply.github.com>
2022-10-01 17:23:56 +0400

    Update serializers.py

--- public_chat/base/serializers.py
+++ public_chat/base/serializers.py
@@ -1,7 +1,7 @@
 from dataclasses import field
 from rest_framework.serializers import ModelSerializer
 from .models import Post
-
+#comment from mac
 class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
